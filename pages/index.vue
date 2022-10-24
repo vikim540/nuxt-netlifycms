992fd7af1e7219c51ea60f4601495653f47fec70
vikim <asd540680736@outlook.com>
2022-10-24 10:43:47 +0800

    Changes

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,7 @@
       <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
       <posts post-type="blog" :amount="1" />
     </section>
+
+
   </main>
 </template>
